d2f6201ac189824ee22d1b9d34a91666ce38d62c
Joshua Pease <joshua.pease@viget.com>
2021-01-29 16:08:22 -0800

    [#14] Connect search to toggle sections that have results

--- src/resources/js/parts-kit.js
+++ src/resources/js/parts-kit.js
@@ -9,107 +9,6 @@ const keyCodes = {
     f: 70,
 };
 
-// Parts kit class for expanding/collapsing sidebar
-class PartsKit {
-    constructor(el) {
-        this.el = el;
-        this.setVars();
-        this.bindEvents();
-        this.initSidebar();
-    }
-
-    setVars() {
-        this.sidebar = this.el.querySelector("[data-sidebar]");
-        this.lsSidebar = "pk-sidebar-hidden";
-        this.fullscreenExpandBtn = this.el.querySelector(
-            "[data-fullscreen-expand]"
-        );
-        this.fullscreenCloseBtn = this.el.querySelector(
-            "[data-fullscreen-close]"
-        );
-        /** @type {HTMLInputElement} */
-        this.sidebarSearch = this.el.querySelector("[data-sidebar-search]");
-        /** @type {NodeListOf<HTMLElement>} */
-        this.sectionItems = this.el.querySelectorAll("[data-section-item]");
-        this.searchList = [...this.sectionItems].map(el => {
-            return {
-                el,
-                searchText: el.getAttribute('data-search-text')?.toLowerCase(),
-                toggle: el.closest("[data-parts-kit-toggle]")
-            }
-        });
-    }
-
-    bindEvents() {
-        document.addEventListener("keydown", this.handleKeydown);
-        this.fullscreenExpandBtn.addEventListener("click", this.toggleSidebar);
-        this.fullscreenCloseBtn.addEventListener("click", this.toggleSidebar);
-        this.sidebarSearch.addEventListener("input", this.handleSearchInput);
-    }
-
-    handleKeydown = ({ keyCode }) => {
-        if (keyCode === keyCodes.f || keyCode === keyCodes.esc) {
-            // TODO don't toggle sidebar if search input is in focus.
-            this.toggleSidebar();
-        }
-    };
-
-    handleSearchInput = (e) => {
-        this.doSearch(this.sidebarSearch.value);
-    }
-
-    clearSearch() {
-        this.sidebar.classList.remove("search-active");
-        this.searchList.forEach(item => {
-            item.el.classList.remove("in-search");
-            item.toggle.classList.remove("in-search");
-        });
-    }
-
-    /**
-     *
-     * @param {String} searchText
-     */
-    doSearch(searchText) {
-        const formattedText = searchText.trim().toLowerCase();
-
-        this.clearSearch()
-
-        if(!formattedText) {
-            return;
-        }
-
-        this.sidebar.classList.add("search-active");
-
-        const searchResults = this.searchList
-            .filter(x => x.searchText.indexOf(searchText) > -1);
-
-        searchResults.forEach(item => {
-            item.el.classList.add("in-search");
-            item.toggle.classList.add("in-search");
-        })
-    }
-
-    initSidebar() {
-        const isHidden = localStorage.getItem(this.lsSidebar) === "true";
-        this.sidebar.classList.toggle("hidden", isHidden);
-        this.fullscreenExpandBtn.classList.toggle("hidden", isHidden);
-        this.fullscreenCloseBtn.classList.toggle("hidden", !isHidden);
-    }
-
-    toggleSidebar = () => {
-        const isHidden = this.sidebar.classList.toggle("hidden");
-        this.fullscreenExpandBtn.classList.toggle("hidden");
-        this.fullscreenCloseBtn.classList.toggle("hidden");
-
-        localStorage.setItem(this.lsSidebar, isHidden);
-    };
-}
-
-// Init the parts kit class
-const partsKitEl = document.querySelector("[data-parts-kit]");
-new PartsKit(partsKitEl);
-
 // Slide class for animating hiding/showing
 class Slide {
     constructor(el, collapsedClass = "collapsed") {
@@ -157,6 +56,7 @@ class Slide {
 }
 
 // Toggle class for sidebar navigation
+// Initialized in our PartsKit class
 class Toggle {
     constructor(el) {
         this.el = el;
@@ -200,6 +100,7 @@ class Toggle {
     };
 
     close = () => {
+        if(!this.isOpen) return;
         this.isOpen = false;
         this.togglee.close();
         this.togglers.forEach((el) =>
@@ -211,6 +112,7 @@ class Toggle {
     };
 
     open = () => {
+        if(this.isOpen) return;
         this.isOpen = true;
         this.togglee.open();
         this.togglers.forEach((el) => el.setAttribute("aria-expanded", "true"));
@@ -230,6 +132,118 @@ class Toggle {
     };
 }
 
-// Init the toggle class
-const toggles = document.querySelectorAll("[data-parts-kit-toggle]");
-toggles.forEach((el) => new Toggle(el));
+// Parts kit class for expanding/collapsing sidebar
+class PartsKit {
+    constructor(el) {
+        this.el = el;
+        this.setVars();
+        this.bindEvents();
+        this.initSidebar();
+    }
+
+    setVars() {
+        this.sidebar = this.el.querySelector("[data-sidebar]");
+        this.lsSidebar = "pk-sidebar-hidden";
+        this.fullscreenExpandBtn = this.el.querySelector(
+            "[data-fullscreen-expand]"
+        );
+        this.fullscreenCloseBtn = this.el.querySelector(
+            "[data-fullscreen-close]"
+        );
+
+        // Set up toggle classes
+        const toggleElements = document.querySelectorAll("[data-parts-kit-toggle]");
+        this.toggles = [...toggleElements].map(el => new Toggle(el));
+
+        /** @type {HTMLInputElement} */
+        this.sidebarSearch = this.el.querySelector("[data-sidebar-search]");
+        /** @type {NodeListOf<HTMLElement>} */
+        this.sectionItems = this.el.querySelectorAll("[data-section-item]");
+        this.searchList = [...this.sectionItems].map(el => {
+            const toggleEl = el.closest("[data-parts-kit-toggle]");
+            return {
+                el,
+                searchText: el.getAttribute('data-search-text')?.toLowerCase(),
+                toggleEl,
+                toggle: this.toggles.find(x => x.el === toggleEl)
+            }
+        });
+    }
+
+    bindEvents() {
+        document.addEventListener("keydown", this.handleKeydown);
+        this.fullscreenExpandBtn.addEventListener("click", this.toggleSidebar);
+        this.fullscreenCloseBtn.addEventListener("click", this.toggleSidebar);
+        this.sidebarSearch.addEventListener("input", this.handleSearchInput);
+    }
+
+    focusInInput = () => document.activeElement.tagName === 'INPUT'
+
+    handleKeydown = ({ keyCode }) => {
+        if ((keyCode === keyCodes.f || keyCode === keyCodes.esc) && !this.focusInInput()) {
+            this.toggleSidebar();
+        }
+    };
+
+    handleSearchInput = (e) => {
+        this.doSearch(this.sidebarSearch.value);
+    }
+
+    clearSearch() {
+        this.sidebar.classList.remove("search-active")
+        this.searchList.forEach(item => {
+            item.el.classList.remove("in-search")
+            item.toggleEl.classList.remove("in-search")
+
+            if (item.toggleEl.hasAttribute('data-active-section')) {
+                item.toggle.open();
+            } else {
+                item.toggle.close()
+            }
+        });
+    }
+
+    /**
+     *
+     * @param {String} searchText
+     */
+    doSearch(searchText) {
+        const formattedText = searchText.trim().toLowerCase();
+
+        this.clearSearch()
+
+        if(!formattedText) {
+            return;
+        }
+
+        this.sidebar.classList.add("search-active");
+
+        const searchResults = this.searchList
+            .filter(x => x.searchText.indexOf(searchText) > -1);
+
+        searchResults.forEach(item => {
+            item.el.classList.add("in-search");
+            item.toggleEl.classList.add("in-search");
+            item.toggle.open();
+        })
+    }
+
+    initSidebar() {
+        const isHidden = localStorage.getItem(this.lsSidebar) === "true";
+        this.sidebar.classList.toggle("hidden", isHidden);
+        this.fullscreenExpandBtn.classList.toggle("hidden", isHidden);
+        this.fullscreenCloseBtn.classList.toggle("hidden", !isHidden);
+    }
+
+    toggleSidebar = () => {
+        const isHidden = this.sidebar.classList.toggle("hidden");
+        this.fullscreenExpandBtn.classList.toggle("hidden");
+        this.fullscreenCloseBtn.classList.toggle("hidden");
+
+        localStorage.setItem(this.lsSidebar, isHidden);
+    };
+}
+
+// Init the parts kit class
+const partsKitEl = document.querySelector("[data-parts-kit]");
+new PartsKit(partsKitEl);

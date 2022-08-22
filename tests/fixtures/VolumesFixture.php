4e95c196e896cf5acc3f2016466316f6d25dd80b
Joshua Pease <joshua.pease@viget.com>
2022-08-22 13:55:00 -0700

    [#51] Cleanup

--- tests/fixtures/VolumesFixture.php
+++ tests/fixtures/VolumesFixture.php
@@ -21,35 +21,4 @@ class VolumesFixture extends ActiveFixture
      * @inheritdoc
      */
     public $depends = [FsFixture::class];
-
-
-    // const BASE_URL = 'http://sample.test/';
-
-    // public $modelClass = Volume::class;
-
-    // public $dataFile = __DIR__ . '/data/volumes.php';
-
-    // public function load():void
-    // {
-    //     parent::load();
-
-    //     // Create the dirs
-    //     foreach ($this->getData() as $data) {
-    //         $settings = Json::decodeIfJson($data['settings']);
-    //         FileHelper::createDirectory($settings['path']);
-    //     }
-
-    //     Craft::$app->set('volumes', new Volumes());
-    // }
-
-    // public function unload():void
-    // {
-    //     // Remove the dirs
-    //     foreach ($this->getData() as $data) {
-    //         $settings = Json::decodeIfJson($data['settings']);
-    //         FileHelper::removeDirectory($settings['path']);
-    //     }
-
-    //     parent::unload();
-    // }
 }
